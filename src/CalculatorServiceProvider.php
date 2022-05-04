b10a09f2b5e46bdc6f6be025d74443333cdb4cda
azhar416 <rar.azhar.416@gmail.com>
2022-05-05 00:38:44 +0700

    service provider

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace azhar\helloworld;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
